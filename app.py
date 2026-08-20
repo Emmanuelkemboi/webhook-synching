53abf643b779cdc22d1db980c0c4c7a08ebc964a
Emmanuelkemboi <m.xavier.freelance@gmail.com>
2026-08-20 13:19:10 +0300

    Send orders through webhook

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def home():
 def create_order():
     product = request.form["product"]
     quantity = request.form["quantity"]
- 
+
     order = {
         "product": product,
         "quantity": quantity
@@ -22,7 +23,14 @@ def create_order():
 
     orders.append(order)
 
+    requests.post(
+        "http://127.0.0.1:5000/webhook",
+        json=order
+    )
+
     return redirect("/")
+
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json()
@@ -32,6 +40,7 @@ def webhook():
     return {
         "message": "Webhook received successfully"
     }, 200
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
