264dab377dc0230d9a299ce0026032f25446e564
Emmanuelkemboi <m.xavier.freelance@gmail.com>
2026-08-20 12:15:31 +0300

    Connect order form to backend

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+orders = []
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", orders=orders)
+
+
+@app.route("/order", methods=["POST"])
+def create_order():
+    product = request.form["product"]
+    quantity = request.form["quantity"]
+
+    order = {
+        "product": product,
+        "quantity": quantity
+    }
+
+    orders.append(order)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
